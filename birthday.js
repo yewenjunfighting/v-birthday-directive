2462561910baf60ae6177634dfca652d51fe3fa0
ywj <1728914873@qq.com>
2018-10-11 08:59:12 +0800

    debug : the problem of display

--- birthday.js
+++ birthday.js
@@ -1,6 +1,10 @@
+let ans = '年龄';
+let getAge = '';
+function clear(it){
+    clearInterval(it);
+}
 Vue.directive('birthday', {
     bind: function(el, binding){
-        let ps = document.querySelector('#ld');
         //获取出生日期
         let bir = binding.value;
         let come = 0;
@@ -21,25 +25,32 @@ Vue.directive('birthday', {
             let dir = new Date().getTime() - come;
             dir /= 1000; //dir是相差的秒数
             if(Math.floor(dir / (3600 * 24)) <= 0){
-                ps.innerHTML = "出生不足一天";
+                ans = "出生不足一天";
             }else if(Math.floor(dir / (3600 * 24) ) < 30){
-                ps.innerHTML = "出生" + Math.floor(dir / (3600 * 24)) + '天';
+                ans = "出生" + Math.floor(dir / (3600 * 24)) + '天';
             }else if(Math.floor(dir / (3600 * 24) / 30) < 12){
-                ps.innerHTML = "出生" + (Math.floor(dir / (3600 * 24) / 30)) + '个月 ';
+                ans = "出生" + (Math.floor(dir / (3600 * 24) / 30)) + '个月 ';
                 if(Math.floor(dir / (24 * 3600)) -  Math.floor(dir / (3600 * 24) / 30) * 30 < 10){
-                    ps.innerHTML = ps.innerHTML + "零" + (Math.floor(dir / (24 * 3600)) -  Math.floor(dir / (3600 * 24) / 30) * 30) + "天"
-                }else ps.innerHTML = ps.innerHTML + ' ' + (Math.floor(dir / (24 * 3600)) -  Math.floor(dir / (3600 * 24) / 30) * 30 )+ "天"
+                    ans = ans + "零" + (Math.floor(dir / (24 * 3600)) -  Math.floor(dir / (3600 * 24) / 30) * 30) + "天"
+                }else ans = ans + ' ' + (Math.floor(dir / (24 * 3600)) -  Math.floor(dir / (3600 * 24) / 30) * 30 )+ "天"
             }else {
-                ps.innerHTML = Math.floor(dir / (3600 * 24) / 30 / 12) + '岁 ';
+                ans = Math.floor(dir / (3600 * 24) / 30 / 12) + '岁 ';
                 if(Math.floor(dir / (24 * 3600) / 30) - Math.floor(dir / (3600 * 24) / 30 / 12)){
-                    ps.innerHTML = ps.innerHTML + ' ' + (Math.floor(dir / (24 * 3600) / 30) - 12 * Math.floor(dir / (3600 * 24) / 30 / 12)) + '个月';
+                    ans = ans + ' ' + (Math.floor(dir / (24 * 3600) / 30) - 12 * Math.floor(dir / (3600 * 24) / 30 / 12)) + '个月';
                 }
                 if(Math.floor(dir / (24 * 3600)) - 30 * Math.floor(dir / (24 * 3600) / 30)){
-                    ps.innerHTML = ps.innerHTML + ' ' + (Math.floor(dir / (24 * 3600)) - 30 * Math.floor(dir / (24 * 3600) / 30)) + '天';
+                    ans = ans + ' ' + (Math.floor(dir / (24 * 3600)) - 30 * Math.floor(dir / (24 * 3600) / 30)) + '天';
                 }
             }
-            console.log(ps);
-            ps.innerHTML = '叶文俊';
+        }else {
+            el.innerHTML = "正在计算你的年龄...";
+            //el.innerHTML = ans;
+            getAge = setInterval(function(){
+                if(ans){
+                    el.innerHTML = '年龄: ' + ans;
+                    clear(getAge); //在计算出结果后把定时器清除掉
+                }
+             }, 100);
         }
     }
 });
